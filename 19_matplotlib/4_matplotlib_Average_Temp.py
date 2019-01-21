0b4937fa11ad9dcb595df915c10291008d09469c
avinash\avinash.t <qwe123kids@gmail.om>
2019-01-20 22:14:20 -0700

    matplotlib

---
+++ 19_matplotlib/4_matplotlib_Average_Temp.py
@@ -0,0 +1,5 @@
+ny_temp= [53.9, 56.3, 56.4,53.4,54.5,55.8,56.8,55.0,55.3,54.0,56.7,57.3]
+
+from pylab import plot, show
+plot(ny_temp, marker='o')
+show()
---
+++ 19_matplotlib/5_matplotlib_Average_Tempand_year_range.py
@@ -0,0 +1,5 @@
+ny_temp= [53.9, 56.3, 56.4,53.4,54.5,55.8,56.8,55.0,55.3,54.0,56.7]
+years = range(2000,2011)
+from pylab import plot, show
+plot(years,ny_temp, marker='o')
+show()
