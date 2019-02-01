6772d51ec5e99b8161e34a3ffab18985122df1b5
avinash\avinash.t <qwe123kids@gmail.om>
2019-01-31 22:54:39 -0700

    data

--- 22_Sympy_Sets/5_Set_Pendulum.py
+++ 22_Sympy_Sets/5_Set_Pendulum.py
@@ -8,9 +8,9 @@ def time_period(length,g):
 if __name__ =='__main__':
     L = FiniteSet(15,18,21,22.5,25)
     g_values = FiniteSet(9.8,9.78,9.83)
-    print('{0:^15}{0:^15}{2:^15}'.format('Length(cm)', 'Gravity(m/s^2)', 'Time Period(s)'))
+    print("{0:^15}{0:^15}{2:^15}".format('Length(cm)', 'Gravity(m/s^2)', 'Time Period(s)'))
     for elem in L*g_values:
         l = elem[0]
         g = elem[1]
         t = time_period(l/100,g)
-        print('{0:^15}{0:^15}{2:^15}'.format(float(l), float(g), float(t)))
+        print("{0:^15}{0:^15}{2:^15}".format(float(l), float(g), float(t)))
