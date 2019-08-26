071688fa538447d05abf5d442dc759d12e27e3f1
AvinashTiwari85 <qwe123kids@gmail.com>
2019-08-25 23:03:16 -0600

    no message

--- 26_sacla_probabilistic_programming/probablistic_program/src/main/scala/learn/avinash/figaro/emailspamdetecter/ReasoningModel.scala
+++ 26_sacla_probabilistic_programming/probablistic_program/src/main/scala/learn/avinash/figaro/emailspamdetecter/ReasoningModel.scala
@@ -2,8 +2,31 @@ package learn.avinash.figaro.emailspamdetecter
 
 import java.util.Dictionary
 
-class ReasoningModel(dictionary: Dictionary,
-                     parameters: LearnedParameters) {
+import com.cra.figaro.language.Flip
+import com.cra.figaro.library.atomic.discrete.Binomial
+import com.cra.figaro.library.compound.If
 
+class ReasoningModel(dictionary: Dictionary,
+                     parameters: LearnedParameters) extends Model(dictionary){
+
+  val isSpam = Flip(parameters.spamProbability)
+
+  val hasWordElements = {
+    for{word <- dictionary.featureWords } yield {
+      val givenSpamProbability = parameters.wordGivenSpamProbabilities(word)
+      val givenNormalProbability = parameters.wordGivenNormalProbabilities(word)
+      val hasWordIfSpam = Flip(givenSpamProbability)
+      val hasWordIfNormal = Flip(givenNormalProbability)
+      val hasWord = If(isSpam, hasWordIfSpam,hasWordIfNormal)(word,hasWord)
+    }
+  }
+
+  val hasManyUnusualIfSpam = Flip(parameters.hasManyUnusualWordsGivenSpamProbability)
+  val hasManyUnusualIfNormal =  Flip(parameters.hasManyUnusualWordsGivenNormalProbability)
+  val hasManyUnusualWords = If(isSpam,hasManyUnusualIfSpam, hasManyUnusualIfNormal)
+
+  val numUnusualIfHasMany = Binomial(Model.binomialNumTrials,parameters.unusualWordGivenManyProbability )
+  val numUnusualIfHasFew = Binomial(Model.binomialNumTrials,parameters.unusualWordGivenFewProbability )
+  val numUnusyalWords = If(hasManyUnusualWords, numUnusualIfHasMany, numUnusualIfHasFew)
 
 }
